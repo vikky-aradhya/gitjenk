8bcd344789ab70630d013318d2e51f188e232c8f
katalontesting <katalontesting1@gmail.com>
2017-09-30 21:17:33 +0530

    added

---
+++ Test Suites/Testsuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testsuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-09-30T21:11:57</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>77f38610-4c56-4b99-a68d-66961443f5f9</testSuiteGuid>
+   <testCaseLink>
+      <guid>04a533c1-47aa-4360-8444-5b8fd3e55314</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
